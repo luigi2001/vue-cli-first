39e9f19beddd30c270305ea4995cf8ceb10b8302
luigi2001 <luigitanga@virgilio.it>
2021-07-05 16:29:34 +0200

    ciao

---
+++ vueCLI/esempio-vue-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Title/>
+    <Hero/>
+  </div>
+</template>
+
+<script>
+import Title from './components/Title.vue'
+import Hero from './components/Hero.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Title,
+    Hero
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vueCLI/esempio-vue-cli/src/components/Hero.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="jumbotron">
+    <h2>jumbotron</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h2{
+  font-size: 50px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.jumbotron{
+  width: 100%;
+  height: 500px;
+  background-color: red;
+}
+</style>
